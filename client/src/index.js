e736307610b223a70b29a85aaddea02b165cfcd6
ayushjadhavvv <ayushjadhav292@gmail.com>
2023-10-14 19:12:29 +0530

    Ayush

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+function App() {
+  const [name, setName] = useState('');
+  
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <div className="container">
+      <h1>Interactive Greeting App</h1>
+      <label htmlFor="nameInput">Enter your name:</label>
+      <input 
+        type="text" 
+        id="nameInput" 
+        value={name} 
+        onChange={handleInputChange} 
+        placeholder="Your Name" 
+      />
+      <div className="greeting">
+        {name && <p>Hello, {name}! Welcome to our open source project!</p>}
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
